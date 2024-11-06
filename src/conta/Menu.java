6cbde2df34b58619045ea0a60cc21bfcfc3edc45
Lyncoln Campello <lyncolncamp@gmail.com>
2024-11-06 18:14:38 -0300

    Segunda tesk

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,11 +1,24 @@
 package conta;
 
-import conta.util.Cores;
+
 import java.util.Scanner;
+import conta.model.Conta;
+import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
+		
+		
+		// teste da classe conta
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f); {
+			c1.visualizar();
+			c1.sacar(12000.0f);
+			c1.visualizar();
+			c1.depositar(5000.0f);
+			c1.visualizar();
+		}
+		
 		int opcao;
 		Scanner leia = new Scanner(System.in);
 
